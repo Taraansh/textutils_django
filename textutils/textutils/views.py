40db7a33729c1c7f59830f3ddbf079628664a585
Taraansh <taraanshnandwani123@gmail.com>
2023-02-04 12:36:42 +0530

    Added new link

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('''<h1>hello world</h1> <a href="https://www.youtube.com/watch?v=AepgWsROO4k&list
-    =PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=7">Django Tutorial</a> ''')
+    =PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=7">Django Tutorial</a> <br> <a 
+    href="https://www.sololearn.com/learning/1073">SoloLearn</a>''')
 
 
 def about(request):
