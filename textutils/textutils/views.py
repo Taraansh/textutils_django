5d0170ce03f5a474ab5b7abbb7829c87d36a1c11
Taraansh <taraanshnandwani123@gmail.com>
2023-02-06 23:26:20 +0530

    Added functions to new checkboxes

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -11,33 +11,51 @@ def index(request):
 def analyze(request):
     # get the text
     djtext = request.GET.get('text', 'default')
+
+    # check checkbox value
     removepunc = request.GET.get('removepunc', 'off')
-    print(removepunc)
-    print(djtext)
+    fullcaps = request.GET.get('fullcaps', 'off')
+    newlineremover = request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    charcounter = request.GET.get('charcounter', 'off')
+
+    # check which checkbox is on
     if removepunc == "on":
-        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~`'''
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~`=+-'''
         analyzed = ""
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
-
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        # Analyze the text
         return render(request, 'analyze.html', params)
+
+    elif fullcaps == 'on':
+        analyzed = ''
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'CAPITALIZED', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif newlineremover == 'on':
+        analyzed = ''
+        for char in djtext:
+            if char != '\n':
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed New Lines', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif extraspaceremover == 'on':
+        analyzed = ''
+        for index, char in enumerate(djtext):
+            if index + 1 < len(djtext) and (not (djtext[index] == " " and djtext[index + 1] == " ")):
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Extra Spaces', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif charcounter == 'on':
+        analyzed = len(djtext)
+        params = {'purpose': 'Character Counter', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
     else:
         return HttpResponse('Error')
-
-# def capfirst(request):
-#     return HttpResponse('capitalize first <button><a href="/">Back</a></button> ')
-#
-#
-# def newlineremove(request):
-#     return HttpResponse('new line remove <button><a href="/">Back</a></button> ')
-#
-#
-# def spaceremove(request):
-#     return HttpResponse('space remove <button><a href="/">Back</a></button> ')
-#
-#
-# def charcount(request):
-#     return HttpResponse('charcount <button><a href="/">Back</a></button> ')
