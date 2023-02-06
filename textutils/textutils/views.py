9deae74d954b623c9656e8f16578cfa8e4e45a54
Taraansh <taraanshnandwani123@gmail.com>
2023-02-06 20:24:39 +0530

    Modified the func to analyze func and used GET requests

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -8,25 +8,36 @@ def index(request):
     # return HttpResponse("Home")
 
 
-def removepunc(request):
+def analyze(request):
     # get the text
     djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    print(removepunc)
     print(djtext)
-    # Analyze the text
-    return HttpResponse('remove punc <button><a href="/">Back</a></button> ')
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~`'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
 
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse('Error')
 
-def capfirst(request):
-    return HttpResponse('capitalize first <button><a href="/">Back</a></button> ')
-
-
-def newlineremove(request):
-    return HttpResponse('new line remove <button><a href="/">Back</a></button> ')
-
-
-def spaceremove(request):
-    return HttpResponse('space remove <button><a href="/">Back</a></button> ')
-
-
-def charcount(request):
-    return HttpResponse('charcount <button><a href="/">Back</a></button> ')
+# def capfirst(request):
+#     return HttpResponse('capitalize first <button><a href="/">Back</a></button> ')
+#
+#
+# def newlineremove(request):
+#     return HttpResponse('new line remove <button><a href="/">Back</a></button> ')
+#
+#
+# def spaceremove(request):
+#     return HttpResponse('space remove <button><a href="/">Back</a></button> ')
+#
+#
+# def charcount(request):
+#     return HttpResponse('charcount <button><a href="/">Back</a></button> ')
