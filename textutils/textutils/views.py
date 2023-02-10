5db378338621963f4c6f191bf25d9b80a9a92298
Taraansh <taraanshnandwani123@gmail.com>
2023-02-10 11:50:09 +0530

    Removed char counter and added if for error

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -17,7 +17,6 @@ def analyze(request):
     fullcaps = request.POST.get('fullcaps', 'off')
     newlineremover = request.POST.get('newlineremover', 'off')
     extraspaceremover = request.POST.get('extraspaceremover', 'off')
-    charcounter = request.POST.get('charcounter', 'off')
 
     # check which checkbox is on
     if removepunc == "on":
@@ -27,38 +26,38 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif fullcaps == 'on':
+    if fullcaps == 'on':
         analyzed = ''
         for char in djtext:
             analyzed = analyzed + char.upper()
         params = {'purpose': 'CAPITALIZED', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif newlineremover == 'on':
+    if newlineremover == 'on':
         analyzed = ''
         for char in djtext:
             if char != '\n' and char != '\r':
                 analyzed = analyzed + char
         params = {'purpose': 'Removed New Lines', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif extraspaceremover == 'on':
+    if extraspaceremover == 'on':
         analyzed = ''
         for index, char in enumerate(djtext):
             if index + 1 < len(djtext) and (not (djtext[index] == " " and djtext[index + 1] == " ")):
                 analyzed = analyzed + char
         params = {'purpose': 'Removed Extra Spaces', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif charcounter == 'on':
-        analyzed = len(djtext)
-        params = {'purpose': 'Character Counter', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-
-    else:
+    if removepunc != "on" and fullcaps != 'on' and extraspaceremover != 'on' and extraspaceremover != 'on':
         return HttpResponse('Error')
+
+    return render(request, 'analyze.html', params)
 
 
 def aboutus(request):
